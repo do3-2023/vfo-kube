2b7b3d8c5c0e792e0d8cd1880189a2e2446254d1
Hunh0w <vincent.font@etu.umontpellier.fr>
2023-07-25 21:57:46 +0200

    update(front): SSR with Spring boot

---
+++ front/src/test/java/fr/hunh0w/front/FrontApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hunh0w.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
